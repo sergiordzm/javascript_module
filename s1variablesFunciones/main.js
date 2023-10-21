0280ddfcdbc312e6d69429856334653d74cc934d
Sergio Rodriguez <sergio.rodriguez@eumex-connect.eu>
2023-10-21 02:38:21 -0600

    avance ejercicio

---
+++ s1variablesFunciones/main.js
@@ -0,0 +1,16 @@
+console.log("Hola, Koders");
+
+let a = 26;
+let b = 13;
+
+let resultado1 = a + b;
+let resultado2 = a - b;
+let resultado3 = a * b;
+let resultado4 = a / b;
+let resultado5 = a % b;
+
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
+console.log(resultado5);
