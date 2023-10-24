10268d927265e3cd3c30f1d062c389d024bc2ea9
Sergio Rodriguez <sergio.rodriguez@eumex-connect.eu>
2023-10-23 22:10:22 -0600

    updates

---
+++ s2operadores/main.js
@@ -0,0 +1,31 @@
+/*Operadores aritméticos*/
+/*
+adicion -> + 
+sustracción -> -
+multiplicación -> *
+división -> /
+módulo -> %
+
+a + b
+a - b
+a * b
+a / b 
+a % b
+
+*/
+
+let a = 8;
+let b = 16;
+let c = 12;
+
+let result = 0;
+
+/*result = result + a;
+result = result + b;
+result = result + c;
+
+console.log(result);*/
+
+result += 8;
+
+console.log(result);
