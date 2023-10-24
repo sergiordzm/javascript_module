2f922f1236760231a6b0fd45af6338a5109041fb
Sergio Rodriguez <sergio.rodriguez@eumex-connect.eu>
2023-10-23 22:31:22 -0600

    updates

--- s2operadores/main.js
+++ s2operadores/main.js
@@ -14,6 +14,19 @@ a % b
 
 */
 
+/*Operadores de asignación*/
+
+/*
+
+Asignación aditiva -> +=
+Asignación sustractiva -> -=
+Asignación multiplicativa -> *=
+Asignación divisiva -> /=
+Asignación modulativa -> %=
+Asignación -> =
+
+*/
+
 /*let a = 8;
 let b = 16;
 let c = 12;
@@ -39,3 +52,18 @@ console.log(result);
 result -= 1;
 
 console.log(result);
+
+/*Operadores de comparación*/
+
+/*
+    Igual a (igualdad) ->  A == B ->  
+        (Compara que el valor de A sea igual al valor de B))
+    Idéntico a (extrema comparación) -> A === B -> 
+        (Compara que tanto el valor como el tipo de dato de A sean iguales al de B)
+    Mayor que ->   A > B -> Evalua que el valor de A sea mayor que el de B
+    Menor que ->   A < B -> Evalua que el valor de A sea menor que el de B
+    Mayor o igual que ->   A >= B -> Evalua que el valor de A sea mayor o igual que el de B
+    Menor o igual que ->   A <= B -> Evalua que el valor de A sea menor o igual que el de B
+    Diferente de -> A != B -> Evalua si el valor de A es diferente que el de B
+    Diferente de -> A !== B -> Evalua si el valor y el tipo de dato de A es diferente que el de B
+*/
