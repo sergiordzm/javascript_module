29acdb9248e6653881bd10c4aed9cc0197dace03
Sergio Rodriguez <sergio.rodriguez@eumex-connect.eu>
2023-10-24 23:02:19 -0600

    exercises

--- s2operadores/main.js
+++ s2operadores/main.js
@@ -87,3 +87,71 @@ console.log(A !== B);
     Or -> expression1 || expression2
     not -> !expression
 */
+
+function addTwoNumbers(firstNumber, secondNumber) {
+  let resultado = firstNumber + secondNumber;
+  return resultado;
+}
+
+function multTwoNumbers(firstNumber, secondNumber) {
+  let resultado = firstNumber * secondNumber;
+  return resultado;
+}
+
+function divTwoNumbers(firstNumber, secondNumber) {
+  let resultado = firstNumber / secondNumber;
+  return resultado;
+}
+
+function susTwoNumbers(firstNumber, secondNumber) {
+  let resultado = firstNumber - secondNumber;
+  return resultado;
+}
+
+function moduleTwoNumbers(firstNumber, secondNumber) {
+  let resultado = firstNumber % secondNumber;
+  return resultado;
+}
+
+let suma1 = addTwoNumbers(6, 9);
+let suma2 = addTwoNumbers(10, 20);
+let multi1 = multTwoNumbers(6, 9);
+let div1 = divTwoNumbers(18, 2);
+let sus1 = susTwoNumbers(18, 5);
+let module1 = moduleTwoNumbers(20, 5);
+
+console.log(suma1);
+console.log(suma2);
+console.log(multi1);
+console.log(div1);
+console.log(sus1);
+console.log(module1);
+
+let superSuma = addTwoNumbers(suma1, suma2);
+
+console.log(superSuma);
+
+/* Crear una función que me diga cual es el mayor de dos números*/
+
+function majorNumber(firstNumber, secondNumber) {
+  if (firstNumber > secondNumber) {
+    return firstNumber;
+  } else {
+    return secondNumber;
+  }
+}
+let first = 18;
+let second = 20;
+let major1 = majorNumber(first, second);
+console.log("El número mayor es:" + major1);
+
+/*crear una función que me diga si un número es par o non*/
+
+let num1 = parOrNon(122);
+function parOrNon(number) {
+  if (number % 2 == 0) {
+    return console.log("El número es par.");
+  } else {
+    return console.log("El número es impar.");
+  }
+}
